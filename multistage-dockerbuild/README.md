626013b206187863f629e13573e3f2994cf9fbeb
vrenigunta <root@ip-172-31-18-249.ec2.internal>
2023-10-23 20:04:01 +0000

    first commit

---
+++ multistage-dockerbuild/README.md
@@ -0,0 +1,5 @@
+# Multi Stage Docker Build
+
+The main purpose of this is to reduce the container image size using multi stage docker build.
+
+To avoid OS and security related vulnerabilities.
---
+++ multistage-dockerbuild/dockerfile-without-multistage/PythonCal/calc.py
@@ -0,0 +1,50 @@
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Main calculator loop
+while True:
+    # User menu
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+
+    # User input
+    user_input = input(": ")
+
+    # Check user choice and perform the corresponding operation
+    if user_input == "quit":
+        break
+    elif user_input in ("add", "subtract", "multiply", "divide"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        if user_input == "add":
+            print("Result:", add(num1, num2))
+        elif user_input == "subtract":
+            print("Result:", subtract(num1, num2))
+        elif user_input == "multiply":
+            print("Result:", multiply(num1, num2))
+        elif user_input == "divide":
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input. Please try again.")
+
+# End of the program
+
